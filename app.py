e1c1f4d87246ddb8aa0aae37fa38f2b9782bfd80
Rizal Ilham <rzlilhm09@gmail.com>
2020-07-30 20:06:44 +0700

    Update Sever

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'rizal_ilham'
-app.config['MYSQL_PASSWORD'] = 'helloworld!'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'flaskdb'
 
 mysql = MySQL(app)
