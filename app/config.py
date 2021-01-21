ad4ce6b154802dee07d541a90ca95b488cfd20fb
rizalilhamm <rzlilhmm09@gmail.com>
2021-01-21 20:52:36 +0700

    Perbaikan pada Configurasi

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('HEROKU_POSTGRESQL_CYAN_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
