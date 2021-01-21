695779bee22f5cea6bbe154c4cf0d71989c2e9c7
rizalilhamm <rzlilhmm09@gmail.com>
2021-01-21 21:30:37 +0700

    -

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY') 
     SQLALCHEMY_DATABASE_URI = os.getenv('HEROKU_POSTGRESQL_CYAN_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
