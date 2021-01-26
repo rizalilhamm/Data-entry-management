e60806e69c35c76a8e476aee7c9978c15ac7b564
rizalilhamm <rzlilhmm09@gmail.com>
2021-01-26 15:19:56 +0700

    Install flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from app import views
+
+# Tambahkan Komentar
