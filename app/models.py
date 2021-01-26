17687bc9b215320c4be895266e85af326fa71081
rizalilhamm <rzlilhmm09@gmail.com>
2021-01-26 15:10:23 +0700

    Create User Model

--- app/models.py
+++ app/models.py
@@ -1,7 +1,16 @@
 from app import db
-
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), index=True, nullable=False)
     description = db.Column(db.String(120), index=True, nullable=False)
     status = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return self.title
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), index=True, nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return self.name
