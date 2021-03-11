d838f19fea13c307d38d6524481490a267f0bd73
Rizal Ilham <rzlilhm09@gmail.com>
2021-03-11 22:24:04 +0700

    Update Configuration

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY') or 'ini-rahasia'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgres://ubuiqvveydpbaj:91fdc5b92df19a8cf3cbfefca133c11c8532d54a5fd8d935dd2336e3bded5d28@ec2-52-45-140-104.compute-1.amazonaws.com:5432/d45ac0kk3kpa72'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgres://ypfwhduleiexbv:072e0b11baaa33481161caee5165711eaadddd9fa0399fa7698134d33ba414fc@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d4rmigt1rhqqub'
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') or True
