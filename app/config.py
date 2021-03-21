12470c00f7810ab18a81aa1afd4a8593fcdde82f
Rizal Ilham <rzlilhm09@gmail.com>
2021-03-21 20:08:01 +0700

    Tambahan User update dan pergantian password

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    heroku_db = 'postgres://cxvgwzcndxklsb:6f2faee447fe6b76854f17391a5fdcea0ec2052be4ecc540bfad5c4f918cc2df@ec2-34-201-248-246.compute-1.amazonaws.com:5432/d9frgf90qs3occ'
     SECRET_KEY = os.getenv('SECRET_KEY') or 'ini-rahasia'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgres://ypfwhduleiexbv:072e0b11baaa33481161caee5165711eaadddd9fa0399fa7698134d33ba414fc@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d4rmigt1rhqqub'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or heroku_db
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') or True
--- app/user/user.py
+++ app/user/user.py
@@ -1,20 +1,20 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import current_user, login_required
 
 from app import app, db
 
 
-@app.route('/profile/<string:name>')
-def user_profile(name):
-    return render_template('user_profile.html', name=name, title=name)
+@app.route('/profile')
+def user_profile():
+    return render_template('user_profile.html')
 
-@app.route('/update/<string:name>', methods=('POST', 'GET'))
+@app.route('/profile/update/<string:name>', methods=('POST', 'GET'))
 def update_profile(name):
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
     if current_user.name != name:
         return redirect(url_for('profile/<string:name>')) 
-
 
     form = request.form
     name = form.get('name')
@@ -23,13 +23,39 @@ def update_profile(name):
     if request.method == "POST":
         current_user.name = name
         current_user.about = about
-        if email != current_user.email:
-            flash('Email ga bisa di ubah')
-            return redirect(url_for('update_profile'))
         db.session.commit()
-        flash('Profile Updated successfully!')
-        return redirect(url_for('profile/<string:name>'))
+        flash("Profile berhasil di update")
+        return redirect(url_for('user_profile', name=current_user.name))
     
     return render_template('update_profile.html', title='Update Profile')
 
+@app.route('/profile/change-password', methods=('POST', 'GET'))
+@login_required
+def change_password():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
     
+    form = request.form
+    password_lama = form.get('password_lama')
+    password_baru = form.get('password_baru')
+    konfirmasi_password_baru = form.get('konfirmasi_password_baru')
+
+    if request.method == "POST":
+        # jika false maka ulangi
+        if not check_password_hash(current_user.password, password_lama):
+            flash("Masih ada yang salah")
+            return redirect(url_for('change_password'))
+        # jika field masih ada yang kosong maka ulangi
+        if not password_baru or not konfirmasi_password_baru:
+            flash("Password baru/lama masih ada yang kosong")
+            return redirect(url_for('change_password'))
+        # jika tidak kosong kosong maka bandingkan dan commit pada server
+        if password_baru == konfirmasi_password_baru:
+            if password_lama == password_baru:
+                return redirect(url_for('user_profile'))
+            current_user.password = generate_password_hash(password_baru)
+            db.session.commit()
+            flash("Password sudah diperbaharui")
+            return redirect(url_for('user_profile'))
+
+    return render_template('change_password.html', title='Change Password')
